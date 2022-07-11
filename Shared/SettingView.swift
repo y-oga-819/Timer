89bbbd7edeffaeffd5abb3285e348c6f229e62b2
yuki819 <yuki819@mba2018.local>
2022-07-12 00:52:13 +0900

    feature(Timer): Pickerで選択した秒数を表示するTextを追加した

--- Shared/SettingView.swift
+++ Shared/SettingView.swift
@@ -16,14 +16,23 @@ struct SettingView: View {
                 .edgesIgnoringSafeArea(.all)
 
             VStack {
+                Spacer()
+                
+                Text("\(timerValue) 秒")
+                    .font(.largeTitle)
+
+                Spacer()
+
                 Picker(selection: $timerValue, label: Text("選択")) {
-                    Text("10").tag(10)
-                    Text("20").tag(20)
-                    Text("30").tag(30)
-                    Text("40").tag(40)
-                    Text("50").tag(50)
-                    Text("60").tag(60)
+                    Text("10秒").tag(10)
+                    Text("20秒").tag(20)
+                    Text("30秒").tag(30)
+                    Text("40秒").tag(40)
+                    Text("50秒").tag(50)
+                    Text("60秒").tag(60)
                 }
+
+                Spacer()
             }
         }
     }
