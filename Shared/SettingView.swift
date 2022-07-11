7908a6ff1acfaeca0f3e4042d37623d35f4ab9be
yuki819 <yuki819@mba2018.local>
2022-07-12 00:48:58 +0900

    feature(Timer): Pickerを追加

--- Shared/SettingView.swift
+++ Shared/SettingView.swift
@@ -8,11 +8,25 @@
 import SwiftUI
 
 struct SettingView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+    @State var timerValue = 10;
 
+    var body: some View {
+        ZStack {
+            Color("backgroundColor")
+                .edgesIgnoringSafeArea(.all)
+
+            VStack {
+                Picker(selection: $timerValue, label: Text("選択")) {
+                    Text("10").tag(10)
+                    Text("20").tag(20)
+                    Text("30").tag(30)
+                    Text("40").tag(40)
+                    Text("50").tag(50)
+                    Text("60").tag(60)
+                }
+            }
+        }
+    }
 }
 
 struct SettingView_Previews: PreviewProvider {
